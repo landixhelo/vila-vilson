402d522e21389a8612d4482e3ba8a3c6a1bb3cb9
landi <landixhelo02@gmail.com>
2024-05-20 16:57:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const elements = document.querySelectorAll('.fadeinleft');
+const elements1 = document.querySelectorAll('.fade-in');
+const elements2 = document.querySelectorAll('.fadeinright');
+const elements3 = document.querySelectorAll('.fadeindown');
+const options = {
+    root:null,
+    rootMargin: '0px',
+    threshold: .4
+}
+const callbacks = (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('active-left');
+        }
+    });
+}
+const callbacks1 = (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('active');
+        }
+    });
+}
+const callbacks2 = (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('active-right');
+        }
+    });
+}
+const callbacks3 = (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('active-down');
+        }
+    });
+}
+let observer = new IntersectionObserver(callbacks, options);
+let observer1 = new IntersectionObserver(callbacks1, options);
+let observer2 = new IntersectionObserver(callbacks2, options);
+let observer3 = new IntersectionObserver(callbacks3, options);
+elements.forEach(elements =>{
+    observer.observe(elements);
+})
+elements1.forEach(elements1 =>{
+    observer1.observe(elements1);
+})
+elements2.forEach(elements2 =>{
+    observer2.observe(elements2);
+})
+elements3.forEach(elements3 =>{
+    observer3.observe(elements3);
+})
